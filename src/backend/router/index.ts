0e69e974d252aac4b7f6446a3bf47d3c4e7e4af8
Niren <nirentuladhar@gmail.com>
2022-10-09 01:17:25 +1100

    Remove example files

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -2,11 +2,9 @@
 import { t } from "../trpc";
 import { balanceRouter } from "./balance";
 import { categoryRouter } from "./category";
-import { exampleRouter } from "./example";
 import { subCategoryRouter } from "./subcategory";
 
 export const appRouter = t.router({
-  example: exampleRouter,
   category: categoryRouter,
   balance: balanceRouter,
   subCategory: subCategoryRouter,
