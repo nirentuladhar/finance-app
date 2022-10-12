fdf571dedf71498788795a4cfb066b63900ecb37
Niren <nirentuladhar@gmail.com>
2022-10-12 23:40:53 +1100

    Add create balance

--- src/backend/router/balance.ts
+++ src/backend/router/balance.ts
@@ -2,6 +2,24 @@ import { t } from "../trpc";
 import { z } from "zod";
 
 export const balanceRouter = t.router({
+  create: t.procedure
+    .input(
+      z.object({
+        amount: z.number().positive(),
+        date: z.date(),
+        subCategoryId: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.balance.create({
+        data: {
+          amount: input.amount,
+          date: input.date,
+          subCategoryId: input.subCategoryId,
+        },
+      });
+    }),
+
   getAll: t.procedure.query(({ ctx }) => {
     return ctx.prisma.balance.findMany({
       include: {
